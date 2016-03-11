3fcc8bf66e173ebaaeea122231c9d58355719f59
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2016-03-11 15:33:26 +0100

    ADD: left column rendered correctly

--- app/assets/javascripts/DetailedViewApp.es6.jsx
+++ app/assets/javascripts/DetailedViewApp.es6.jsx
@@ -1,5 +1,16 @@
 const { $, React, ReactDOM, Redux, ReactRedux,
-        ReactBootstrap: { Panel, Heading, Row, Col, Grid, Button } } = window;
+        ReactBootstrap: { 
+          Alert,
+          Button,
+          Col, 
+          Grid, 
+          Heading, 
+          ListGroup,
+          ListGroupItem,
+          Panel, 
+          Row, 
+        } 
+      } = window;
 
 function DetailedViewApp() {
   const Header = ({conference}) => {
@@ -23,19 +34,52 @@ function DetailedViewApp() {
     </h3>
   );
 
+  const eventsDays = (
+    <h3>
+      <div className="pull-right">
+        <Button href="/conferences/active-conference/conference_days" bsStyle="primary" bsSize="xs">Manage</Button>
+      </div>
+      Days
+    </h3>
+  );
+
   const DetailedView = ({conference}) => {
     return (<Grid>
               <Row>
-                <Header />
+                <Header conference={conference}/>
               </Row>
               <Row>
                 <Col md={6}>
                   <Panel header={eventsTitle}>
-                    Panel content
+                    <ListGroup>
+                      {conference.events.map(({name, speaker, description, durationInMinutes, schedule}) => {
+                        return (
+                          <ListGroupItem key={name}>
+                            <h4>
+                              {name}
+                              <br/>
+                              <small>by {speaker}</small>
+                            </h4>
+                            <h6>Description</h6>
+                            <p>
+                              {description || <i>no description provided</i>}
+                            </p>
+                            <p><i>This event takes {durationInMinutes} minutes.</i></p>
+                            <Alert bsStyle={(schedule.day == null) ? 'warning' : 'success'}>
+                              {(schedule.day == null && schedule.startTime) 
+                                ? "This event is scheduled for Day " + schedule.day + " of the conference at " + schedule.startTime
+                                : "This event is not scheduled yet!"
+                              }
+                            </Alert>
+
+                          </ListGroupItem>
+                          );
+                      })}
+                    </ListGroup>
                   </Panel>
                 </Col>
                 <Col md={6}>
-                  <Panel header="Days">
+                  <Panel header={eventsDays}>
                     Panel content
                   </Panel>
                 </Col>
@@ -45,7 +89,42 @@ function DetailedViewApp() {
   };
 
   const initialState = () => {
-    return { id: "active-conference", key: "wroc_love.rb-2016", name: "wroc_love.rb 2016" };
+    return { 
+      id: "active-conference", 
+      key: "wroc_love.rb-2016", 
+      name: "wroc_love.rb 2016",
+      events: [
+        { 
+          name: "Working with Legacy Code",
+          speaker: 'Andrzej Krzywda',
+          description: "I'll show cool tricks to make your legacy codebase maintainable and make it easy to add new features without introducing regressions.",
+          durationInMinutes: 60,
+          schedule: {
+            day: 1,
+            startTime: "18:00"
+          }
+        },
+        { 
+          name: "React.js + Redux Workshops",
+          speaker: 'Marcin Grzywaczewski',
+          description: "",
+          durationInMinutes: 310,
+          schedule: {
+          }          
+        }
+      ],
+      days: [
+        {
+          key: 'day1',
+          dayNumber: 1,
+          startTime: '2016-03-11 11:00:00 CET',
+          schedule: {
+            day: null,
+            startTime: null
+          }
+        }
+      ]
+    };
   };
 
   // Reducer
